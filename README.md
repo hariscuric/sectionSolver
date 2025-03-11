50a7bf2513446451d6b87363906ec8a43b6aa69b
hariscuric <hariscuric123@gmail.com>
2025-03-11 23:55:23 +0100

    Readme and documentation improved

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sectionSolver
 Code to calculate RC section failure surface, curvature-moment diagrams, bending modulus etc.
+
+Still in development. Currently only able to compute moment-curvature diagram for defined rectangular RC section.
+If main.py is run, program will compute and display moment-curvature diagram for 80x40cm section with 22 fi16 bars symetrically arranged around the section (4 corner, 6 left edge, 6 right edge, 3 top edge and 3 bottom edge bars). Diagram is evaluated for axial load state of 2000kN compresion force.
+
+Program takes in account nonlinear material behaviour (both concrete and reinforcement) according to Eurocode 2. Moment - curvature diagram is computed for fixed axial force level using displacement-control (curvature is controlled) using Newton-Raphson method
+
+Solution algorithm is described in SectionSolver.pdf file
--- main.py
+++ main.py
@@ -7,24 +7,9 @@ def main():
 
     con = sec.concrete(30,1.5)
 
-    # strain = np.arange(-0.006,0.001,0.0001)
-
-    # stress = np.zeros((len(strain)),dtype=float)
-    # stiffness = np.zeros((len(strain)),dtype=float)
-    # for n,i in enumerate(strain):
-    #     stress[n] = con.stress(i)
-    #     stiffness[n] = con.tangentModulus(i)
-
-    # print("strain:")
-    # print(strain)
-    # print("stress:")
-    # print(stress)
-    # print("tangent modulus:")
-    # print(stiffness)
-
     
     steel = sec.steel(420,1.15)
-    b = sec.ordinaryRectangularSection(1000,350,40,1,1,4,4,16,16)
+    b = sec.ordinaryRectangularSection(800,400,40,3,3,6,6,16,16)
     b.assignSectionMaterial(con)
     b.assignRebarMaterial(steel)
     b.assignDiscretisation(100)
@@ -32,23 +17,9 @@ def main():
 
     d, lamb = b.computeM_muDiagram(0.000001,34,-2000000)
 
-    print("d:")
-    print(d)
-    print("lamb:")
-    print(lamb)
-
-    print("Nmm:")
-
-    
-    print("curvature at max 68kNm")
-    print(d[6])
-
-    Nmm = b.computeNMM(d[-1])
-    print("Nmm at curvature of 0.0002/mm")
-    print(Nmm)
 
     plt.plot(d[:,1]*1000, lamb*0.000001)
-    plt.title("M3 moment - curvature graph; Normal force = 0")
+    plt.title("M3 moment - curvature graph; Normal force = -2000kN")
     plt.xlabel("Curvature (1/m)")
     plt.ylabel("M3 moment (kNm)")
     plt.grid()
